b75df15a148cefd4b49556645ce431699d118868
fszabi <forgacsszabolcs@pm.me>
2024-05-10 11:50:34 +0200

    logo changed

--- src/app/Nav.tsx
+++ src/app/Nav.tsx
@@ -8,31 +8,32 @@ import logo from "/public/assets/logo/logo.png";
 import logo_dark from "/public/assets/logo/logo_dark.png";
 
 const Nav = () => {
-  const [mounted, setMounted] = useState(false);
-  const { theme } = useTheme();
+  // const [mounted, setMounted] = useState(false);
+  // const { theme } = useTheme();
 
-  useEffect(() => {
-    setMounted(true);
-  }, []);
+  // useEffect(() => {
+  //   setMounted(true);
+  // }, []);
 
-  if (!mounted) {
-    return null;
-  }
+  // if (!mounted) {
+  //   return null;
+  // }
 
-  const systemTheme =
-    window.matchMedia &&
-    window.matchMedia("(prefers-color-scheme: dark)").matches
-      ? "dark"
-      : "light";
+  // const systemTheme =
+  //   window.matchMedia &&
+  //   window.matchMedia("(prefers-color-scheme: dark)").matches
+  //     ? "dark"
+  //     : "light";
 
-  const themeToUse = theme === "system" ? systemTheme : theme;
+  // const themeToUse = theme === "system" ? systemTheme : theme;
 
   return (
     <nav
       className="flex items-center justify-between p-6 lg:px-8 gap-10"
       aria-label="Navigáció"
     >
-      <div className="flex">
+      <p className="font-semibold">AutoLuxe</p>
+      {/* <div>
         {themeToUse === "light" ? (
           <Image
             className="h-14 w-auto"
@@ -46,7 +47,7 @@ const Nav = () => {
             alt="gosi és kertész services logo dark"
           />
         )}
-      </div>
+      </div> */}
       <ThemeSwitcher />
     </nav>
   );
